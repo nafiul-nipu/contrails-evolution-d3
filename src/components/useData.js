0e9cc40a7767afa031ee7a9c98858ed1a6ab32be
Nafiul Nipu <nafiulnipu.2013@gmail.com>
2021-11-03 19:08:51 -0500

    contrails data added

--- src/components/useData.js
+++ src/components/useData.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import cluster from "../data/cluster2.1.json";
-import network from"../data/network2.1.json"
+import cluster from "../data/contrails1-cluster.json";
+import network from"../data/contrails1-network.json"
 
 export const useData = () =>{
     const [clusterData, setClusterData] = useState(null)
